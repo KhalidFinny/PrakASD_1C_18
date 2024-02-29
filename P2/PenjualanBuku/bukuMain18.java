492e65cd0f4f8291529fa05e794019e96a8c956e
Khalid Atthoriq <katthoriq@gmail.com>
2024-02-29 20:42:55 +0700

    P2_Latihan1_1C_18

--- P2/PenjualanBuku/bukuMain18.java
+++ P2/PenjualanBuku/bukuMain18.java
@@ -3,24 +3,25 @@ package P2.PenjualanBuku;
 public class bukuMain18 {
     public static void main(String[] args) {
 
-        buku18 bk1 = new buku18();
-        bk1.judul = "Today ends Tomorrow Comes";
-        bk1.pengarang = "Denanda Pratiwi";
-        bk1.halaman = 198;
-        bk1.stok = 13;
-        bk1.harga = 71000;
+        // buku18 bk1 = new buku18();
+        // bk1.judul = "Today ends Tomorrow Comes";
+        // bk1.pengarang = "Denanda Pratiwi";
+        // bk1.halaman = 198;
+        // bk1.stok = 13;
+        // bk1.harga = 71000;
 
-        bk1.tampilInformasi();
-        bk1.terjual(5);
-        bk1.gantiHarga(60000);
-        bk1.tampilInformasi();
+        // bk1.tampilInformasi();
+        // bk1.terjual(5);
+        // bk1.gantiHarga(60000);
+        // bk1.tampilInformasi();
 
         buku18 bk2 = new buku18("Self Reward", "Maheera Ayesha", 160, 29, 59000);
-        bk2.terjual(11);
+        bk2.terjual(5);
         bk2.tampilInformasi();
 
-        buku18 bkkhalid = new buku18("ford vs ferrari", "carol shelby", 200, 40, 250000);
-        bkkhalid.terjual(20);
-        bkkhalid.tampilInformasi();
+        // buku18 bkkhalid = new buku18("ford vs ferrari", "carol shelby", 200, 40,
+        // 250000);
+        // bkkhalid.terjual(20);
+        // bkkhalid.tampilInformasi();
     }
 }
