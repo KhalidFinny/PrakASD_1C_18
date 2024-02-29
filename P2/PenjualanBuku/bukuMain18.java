e7feaf00fc6009dab73be43a773477438bc4844f
Khalid Atthoriq <katthoriq@gmail.com>
2024-02-29 10:54:34 +0700

    P2_Percobaan3_1C_18

--- P2/PenjualanBuku/bukuMain18.java
+++ P2/PenjualanBuku/bukuMain18.java
@@ -3,7 +3,7 @@ package P2.PenjualanBuku;
 public class bukuMain18 {
     public static void main(String[] args) {
 
-        buku18 bk1 = new buku18(null, null, 0, 0, 0);
+        buku18 bk1 = new buku18();
         bk1.judul = "Today ends Tomorrow Comes";
         bk1.pengarang = "Denanda Pratiwi";
         bk1.halaman = 198;
