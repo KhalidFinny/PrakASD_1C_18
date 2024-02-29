43e3066048448189da8c13feb92161851db678fb
Khalid Atthoriq <katthoriq@gmail.com>
2024-02-29 10:02:05 +0700

    P2_Percobaan3_1C_18

--- P2/PenjualanBuku/buku18.java
+++ P2/PenjualanBuku/buku18.java
@@ -5,6 +5,14 @@ public class buku18 {
     String judul, pengarang;
     int halaman, stok, harga;
 
+    public buku18(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
--- P2/PenjualanBuku/bukuMain18.java
+++ P2/PenjualanBuku/bukuMain18.java
@@ -3,28 +3,20 @@ package P2.PenjualanBuku;
 public class bukuMain18 {
     public static void main(String[] args) {
 
-        // buku18 bk1 = new buku18();
-        // bk1.judul = "Today ends Tomorrow Comes";
-        // bk1.pengarang = "Denanda Pratiwi";
-        // bk1.halaman = 198;
-        // bk1.stok = 4;
-        // bk1.harga = 71000;
+        buku18 bk1 = new buku18(null, null, 0, 0, 0);
+        bk1.judul = "Today ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
 
-        // bk1.tampilInformasi();
-        // bk1.terjual(5);
-        // bk1.gantiHarga(60000);
-        // bk1.tampilInformasi();
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
 
-        buku18 bk2 = new buku18();
-        bk2.judul = "How to Train Your Dragon";
-        bk2.pengarang = "Denanda Pratiwi";
-        bk2.halaman = 432;
-        bk2.stok = 0;
-        bk2.harga = 275000;
-
-        bk2.tampilInformasi();
-        bk2.terjual(4);
-        bk2.gantiHarga(60000);
+        buku18 bk2 = new buku18("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
         bk2.tampilInformasi();
 
     }
