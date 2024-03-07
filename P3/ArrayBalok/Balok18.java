0343cecc74b137016ac1a0e6f63edfab785c8bb4
Khalid Atthoriq <katthoriq@gmail.com>
2024-03-07 11:23:48 +0700

    P3_Percobaan3_pertanyaan1_1C_18

--- P3/ArrayBalok/Balok18.java
+++ P3/ArrayBalok/Balok18.java
@@ -5,9 +5,20 @@ public class Balok18 {
     public int lebar;
     public int tinggi;
 
+    public Balok18() {
+        panjang = 0;
+        lebar = 0;
+        tinggi = 0;
+    }
+
     public Balok18(int p, int l, int t) {
         panjang = p;
         lebar = l;
+        tinggi = t;
+
+    }
+
+    public Balok18(int t) {
         tinggi = t;
 
     }
